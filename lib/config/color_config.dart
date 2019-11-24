87de7ad14c1b2e1280850000187d2890d838c16d
Yun <yun160308@gmail.com>
2019-11-24 23:46:31 +0800

    feat(ALL): 修改颜色

--- lib/config/color_config.dart
+++ lib/config/color_config.dart
@@ -5,7 +5,9 @@ class ColorConfig {
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0x000000);
   static const Color red = Color(0xFFC91B3A);
+  static const Color china_green = Color(0xFF065279);
   static const Color gray = Color(0xFF888888);
   static const Color textDefault = Color(0xFF888888);
   static const Color textBlack = Color(0xFF313131);
+  static const Color mainTheme = china_green;
 }
